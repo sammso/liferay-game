5a43a40096ab624bab28851d0645ce8239d4ca18
Julio Camarero <julio.camarero@liferay.com>
2017-09-20 18:32:54 +0200

    Add activator that creates 2 pages and the first one has our theme

---
+++ game-theme-activator/src/main/java/com/liferay/game/theme/activator/internal/instance/lifecycle/SetDefaultSiteThemePortalInstanceLifecycleListener.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.game.theme.activator.internal.instance.lifecycle;
+
+import com.liferay.login.web.constants.LoginPortletKeys;
+import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.util.DefaultLayoutPrototypesUtil;
+import com.liferay.portal.util.PortalInstances;
+import com.liferay.portal.util.PropsValues;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eduardo Garcia
+ */
+@Component(immediate = true, service = PortalInstanceLifecycleListener.class)
+public class SetDefaultSiteThemePortalInstanceLifecycleListener
+	extends BasePortalInstanceLifecycleListener {
+
+	@Override
+	public void portalInstanceRegistered(Company company) throws Exception {
+		if (company.getCompanyId() != PortalInstances.getDefaultCompanyId()) {
+			return;
+		}
+
+		Group defaultGroup = _groupLocalService.getGroup(
+			company.getCompanyId(),
+			PropsValues.VIRTUAL_HOSTS_DEFAULT_SITE_NAME);
+
+		Layout defaultLayout = _layoutLocalService.fetchLayout(
+			defaultGroup.getDefaultPublicPlid());
+
+		if (defaultLayout == null) {
+			defaultLayout = DefaultLayoutPrototypesUtil.addLayout(
+				defaultGroup.getPublicLayoutSet(), "home", "/home", "1_column");
+		}
+
+		_layoutLocalService.updateLookAndFeel(
+			defaultLayout.getGroupId(), defaultLayout.getPrivateLayout(),
+			defaultLayout.getLayoutId(), _THEME_ID, "01", null);
+
+		Layout loginLayout = _layoutLocalService.fetchLayoutByFriendlyURL(
+			defaultGroup.getGroupId(), false, "/login");
+
+		if (loginLayout != null) {
+			return;
+		}
+
+		loginLayout = DefaultLayoutPrototypesUtil.addLayout(
+			defaultGroup.getPublicLayoutSet(), "login", "/login", "1_column");
+
+		DefaultLayoutPrototypesUtil.addPortletId(
+			loginLayout, LoginPortletKeys.LOGIN, "column-1");
+	}
+
+	@Reference(unbind = "-")
+	protected void setGroupLocalService(GroupLocalService groupLocalService) {
+		_groupLocalService = groupLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setLayoutLocalService(
+		LayoutLocalService layoutLocalService) {
+
+		_layoutLocalService = layoutLocalService;
+	}
+
+	@Reference(
+		target = "(javax.portlet.name=" + LoginPortletKeys.LOGIN + ")",
+		unbind = "-"
+	)
+	protected void setLoginPortlet(Portlet portlet) {
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=game-theme)", unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+	}
+
+	private static final String _THEME_ID = "game_WAR_gametheme";
+
+	private GroupLocalService _groupLocalService;
+	private LayoutLocalService _layoutLocalService;
+
+}
